b2a284ae0a5a643123087640e02fcfe7dfe4e72f
Fuad010 <fuadtopchiyev@gmail.com>
2024-07-22 03:36:37 +0400

    feat:complete all ui-es

---
+++ src/components/Common/Input/Input.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Input.module.css'
+
+const Input = () => {
+  return (
+    <input className={styles.input} type="text" placeholder="Title"></input>
+  )
+}
+
+export default Input
--- src/components/Common/Todos/Todos.js
+++ src/components/Common/Todos/Todos.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './Todos.module.css'
 import Table from '../Table/Table'
+import Button from '../Button/Button'
+import Input from '../Input/Input'
 
 const Todos = () => {
   return (
@@ -8,6 +10,20 @@ const Todos = () => {
       <Table />
       <Table />
       <Table />
+      <div className="flex gap-[100px] justify-center">
+        <Input />
+        <Input />
+      </div>
+      <div>
+        <Input />
+      </div>
+      <div className="flex gap-[100px] justify-center">
+        <Button />
+        <Button />
+      </div>
+      <div>
+        <Button />
+      </div>
     </div>
   )
 }
